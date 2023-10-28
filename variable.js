bb88747d665216ddb12188553ec5919c7f08d84e
Anuradha0710 <y.anu0710@gmail.com>
2023-10-28 21:27:21 +0530

    js practic

---
+++ variable.js
@@ -0,0 +1,22 @@
+//varible: are the container for storing data/value
+//let/ar/const default var(keywords) followed by VN
+//variable name(VN): rule >>> space not allowed, 
+//first ltr of variable should always alphabets,special characters not allowed (except $ _ )
+
+
+
+
+let fName;   //declartion of variale  //undefined
+fName ='Anuradha' //defination (syntax variableName = value/data)  //diffrence = == ===
+
+let myfirstName = 'Anuradha';  //declaration + defination 
+
+var mySName ='Yadav';
+
+var myName;
+myName ='om';
+myName ='Anu';
+
+const cityName ='Sangli';  //const keyword intialization of variable is must
+//cityName ='pune'
+console.log('cityName',cityName);
