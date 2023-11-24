1a1c91f1a9d0468bde1620a7a005ba50e1182c4f
Anuradha0710 <y.anu0710@gmail.com>
2023-11-24 23:41:09 +0530

    routing

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SingUpComponent } from './sing-up/sing-up.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 @NgModule({
   declarations: [      //compnent/pipe/directive
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SingUpComponent,
+    LogInComponent
   ],
   imports: [           //modules
     BrowserModule,
