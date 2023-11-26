bc88e7b03fefe3f5e14dcc481117627c942e7d09
Anuradha0710 <y.anu0710@gmail.com>
2023-11-26 13:26:28 +0530

    property

--- myApp/src/app/home/home.component.ts
+++ myApp/src/app/home/home.component.ts
@@ -1,16 +1,37 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router } from '@angular/router';        //import section
 
-@Component({
+@Component({       //component directive : meta data of component
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent {         //component class : properties,constructor,lifecycle hooks,methods etc.
+                    //property setion  
+  //let/var/cont city = "pune" var in js
 
-  constructor(private router: Router){}
+
+  city : string = "Satara";  //property
+  surName : any = "234567788"
+  name : string ="Anuradha"
+  name2? : string;                                   //?/!
+  name3 : any
+  constructor(private router: Router){}  //constructor sction
+
+
+  //LH,methods
   signUpCompo(){
+
+    this.name3 = "Shree";
+    console.log("city",this.city);
     console.log("signUpCompo fun calling");
-    this.router.navigateByUrl("SingUp")
+    this.test();
+    this.router.navigateByUrl("SingUp");
+  }
+
+
+  test(){
+    console.log("test calling...");
+    
   }
 }
