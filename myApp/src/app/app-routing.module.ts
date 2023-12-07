6346fab444abaa1d267c2cf217e2668721ddd735
Anuradha0710 <y.anu0710@gmail.com>
2023-12-07 21:23:04 +0530

    lc

--- myApp/src/app/app-routing.module.ts
+++ myApp/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { LogInComponent } from './log-in/log-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { DatabindingComponent } from './databinding/databinding.component';
 import { DirectivesComponent } from './directives/directives.component';
+import { LifecyclehooksComponent } from './lifecyclehooks/lifecyclehooks.component';
 
 const routes: Routes = [
   { path : '', component : HomeComponent},
   { path : "login" , component : LogInComponent},
   { path : "SingUp" , component : SignUpComponent},
   { path : "dataBinding" ,component : DatabindingComponent},
-  { path : "directives" , component : DirectivesComponent}
+  { path : "directives" , component : DirectivesComponent},
+  { path : "lifecycleHooks",component : LifecyclehooksComponent}
 ];
 
 @NgModule({
