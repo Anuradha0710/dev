5b42d515661a818f2dcc5055e33b6b1e67ed22d1
Anuradha0710 <y.anu0710@gmail.com>
2023-12-08 14:47:14 +0530

    LifeCycleHooks

--- myApp/src/app/lifecyclehooks/lifecyclehooks.component.ts
+++ myApp/src/app/lifecyclehooks/lifecyclehooks.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-lifecyclehooks',
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./lifecyclehooks.component.css']
 })
 export class LifecyclehooksComponent {
+
+  name:any; //normal prop
+  // @Input() surName :any; //input prop
+
+
+  constructor(){
+    console.log("constructor calling..");
+    
+  }
+//A component instanc has a lifecycle that starts when Angular instantiates the component class and renders the component views and its child views.
+//1.ngOnChanges()  if component is having @input bound prperties then ngOnChanges lc h get triiger first.
+//2.ngOnInit()
+//3.ngDoCheck()
+//4.ngAfterContentInit()
+//5.ngAfterContentChecked()
+//6.ngAfterViewInit()
+//7.ngAfterViewChecked()
+//8.ngOnDestroy
+
+ngOnChenges(){
+  console.log("OnChenges calling..");
+  
+}
+ngOnInit(){
+  console.log("Oninit calling..");
+  
+}
+ngDoCheck(){
+  console.log("Do Check Calling..");
+  
+}
+ngAfterContentInit(){
+  console.log("AfterContentInit calling..");
+  
+}
+ngAfterContentChecked(){
+  console.log("AfterContentChecked calling..");
+  
+}
+ngAfterViewInit(){
+  console.log("AfterViewInit calling..");
+  
+}
+ngAfterViewChecked(){
+  console.log("AfterViewChecked calling..");
+  
+}
+ngOnDestroy(){
+  console.log("OnDestroy calling");
+  
+}
 
 }
