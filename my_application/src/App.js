1aaacb403925c6401f5f8b136d28a803045d4583
Anuradha0710 <y.anu0710@gmail.com>
2023-12-13 14:25:05 +0530

    website

--- my_application/src/App.js
+++ my_application/src/App.js
@@ -1,10 +1,9 @@
-
+import Navigation from "./component/Navigation";
 
 function App() {
   return (
     <div >
-      <h1>My Application</h1>
-      <img src="https://www.adobe.com/acrobat/hub/media_173d13651460eb7e12c0ef4cf8410e0960a20f0ee.jpeg?width=750&format=jpeg&optimize=medium"/>
+      <Navigation/>
     </div>
   );
 }
