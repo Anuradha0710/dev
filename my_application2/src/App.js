3157a8cc624afd85cc967f0acd6745355a0dfa8c
Anuradha0710 <y.anu0710@gmail.com>
2023-12-16 22:00:23 +0530

    abc

---
+++ my_application2/src/App.js
@@ -0,0 +1,15 @@
+//functional componnent creation
+//javaScript xml => tags
+//only one jsx can be retuned fom a componet
+import imageDisplayer from "./imageDisplayer"
+function App(){
+  return(
+    <div>
+        <h1>Heading</h1>
+        
+    </div>
+
+  // document.createElement('h1')
+  )
+}
+export default App
---
+++ my_application2/src/imageDisplayer.jsx
@@ -0,0 +1,12 @@
+//rafce
+import React from 'react'
+
+const imageDisplayer = () => {
+  return (
+    <div>
+      <img src='https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg' alt='..'/>
+    </div>
+  )
+}
+
+export default imageDisplayer
