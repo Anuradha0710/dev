4dccb2a65acb03db15296fd3492153a275b296ef
Anuradha0710 <y.anu0710@gmail.com>
2023-12-19 21:24:58 +0530

    macFun

--- my_application2/src/PhotoDisplayer.jsx
+++ my_application2/src/PhotoDisplayer.jsx
@@ -1,29 +1,40 @@
 import React from 'react'
-import Img1 from './resources/download.jpg'
-import Img2 from './resources/89170584-bonne-petite-fille-avec-des-feuilles-d-automne-dans-le-parc.jpg'
-import Img3 from './resources/pretty-girl-with-cup-hot-drink-smiles-forest-autumn_116317-383.avif'
-import Img4 from './resources/free-images.avif'
+import arr from './data/images'
 
 
 
 const PhotoDisplayer = () => {
   return (
     <div>
-        <img src={Img1} style=
+
+      {arr.map(function(ele){
+        return  <img src={ele} style=
+        {{width:"200px",
+        height:"200px"}} alt='..'/>
+      })}
+      {arr.map(function(ele){
+        return  <img src={ele} style=
+        {{width:"200px",
+        height:"200px"}} alt='..'/>
+      })}
+
+
+{/* 
+        <img src={arr[0]} style=
         {{width:"200px",
         height:"200px"}} alt='..'/>
 
-      <img src={Img2} style=
+      <img src={arr[1]} style=
         {{width:"200px",
         height:"200px"}} alt='..' />
 
-      <img src={Img3} style=
+      <img src={arr[2]} style=
         {{width:"200px",
         height:"200px"}} alt='..' />
 
-      <img src={Img4} style=
+      <img src={arr[3]} style=
         {{width:"200px",
-        height:"200px"}} alt='..' />
+        height:"200px"}} alt='..' /> */}
 
 
         {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtUslYsdkSfAX
---
+++ my_application2/src/data/images.js
@@ -0,0 +1,8 @@
+import Img1 from '../resources/download.jpg'
+import Img2 from '../resources/89170584-bonne-petite-fille-avec-des-feuilles-d-automne-dans-le-parc.jpg'
+import Img3 from '../resources/pretty-girl-with-cup-hot-drink-smiles-forest-autumn_116317-383.avif'
+import Img4 from '../resources/free-images.avif'
+
+const arr =[Img1,Img2,Img3,Img4]
+
+export default arr;
