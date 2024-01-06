8d5e0ab99f04f089f81e818e1944f3e0931e89ce
Anuradha0710 <y.anu0710@gmail.com>
2024-01-06 23:13:24 +0530

    react

---
+++ todolist/src/App.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import Todo from "./components/Todo";
+
+function App() {
+  // let arr =["task 1","task 2","task 3"]
+
+  const [arr , setArr] = useState(["task 1"])
+  return (
+    <div style={{border:"5px solid red",width:"500px",height:"400px",
+    backgroundColor:"pink",display:"flex",justifyContent:"center",alignItems:"center"}}>
+      <Todo task={arr} setArr={setArr}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ todolist/src/components/Todo.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Todo({props}) {
+
+   let inputval ="";
+  return (
+    <div style={{border:"5px solid blue",backgroundColor:"gray",width:"300px"
+    ,height:"300px"}}>
+      <input onChange={function(event){
+        inputval =event.target.value
+      }}/>
+      <button onClick={function(event){
+        props.setArr([...props.task,inputval])
+      }}>Add</button>
+
+      <div>
+        {props.task.map(function(ele){
+            return <p style={{marginLeft:"20px",color:"black",fontWeight:"900",fontSize:"25px"}}>
+                {ele}
+                </p>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Todo
